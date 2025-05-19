87af369054a2f0b92c6dff27f574794cfdd2ab7c
Zaidan Agusta <zaidanagusta319@gmail.com>
2025-05-19 03:47:17 +0000

    mojito: drop hardware/xiaomi
    Signed-off-by: Zaidan Agusta <zaidanagusta319@gmail.com>

--- device.mk
+++ device.mk
@@ -465,9 +465,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/qcom-caf/bootctrl \
-    hardware/xiaomi
-
+    hardware/qcom-caf/bootctrl 
+    
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
